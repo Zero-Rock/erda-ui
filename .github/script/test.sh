7a2a22dcdbcaa1d8d96fec204af8405abe899875
Zero <mobius_pan@yeah.net>
2021-04-23 14:35:09 +0800

    chore: test action

---
+++ .github/script/test.sh
@@ -0,0 +1,18 @@
+cd ../../core
+echo 当前目录: $(pwd)
+
+echo install dependence for core
+
+npm install
+
+cd ../shell
+
+echo 当前目录: $(pwd)
+
+echo install dependence for core
+
+npm install
+
+npm run test
+
+cp -R ./coverage ../coverage
