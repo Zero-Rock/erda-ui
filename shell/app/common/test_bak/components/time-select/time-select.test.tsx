8bb9192f606c50417ebe71213201cd130671d5b0
Zero <mobius_pan@yeah.net>
2022-03-15 14:10:42 +0800

    feat(test): add TimeSelect test case

--- shell/app/common/test_bak/components/time-select/time-select.test.tsx
+++ shell/app/common/test_bak/components/time-select/time-select.test.tsx
@@ -22,43 +22,12 @@ describe('time-select', () => {
     const renderTimeSelect = (props: IProps): ReactWrapper<IProps, {}, React.Component> => {
       return mount(<TimeSelect {...props} />);
     };
-    it('TimeSelect should trigger change when mounted', () => {
-      const changeFn = jest.fn();
-      const wrapper = renderTimeSelect({
-        onChange: changeFn,
-        triggerChangeOnMounted: true,
-        defaultValue: { mode: 'quick', quick: 'days:1', customize: {} },
-      });
-      expect(wrapper.find('.time-range').text()).toBe('last 1 days');
-      expect(changeFn).toHaveBeenCalledTimes(1);
-      wrapper.find('.manual-refresh').children().simulate('click');
-      expect(changeFn).toHaveBeenCalledTimes(2);
-    });
-    it('TimeSelect manual refresh should work well', () => {
-      const changeFn = jest.fn();
-      const wrapper = renderTimeSelect({ onChange: changeFn });
-      wrapper.find('.manual-refresh').children().simulate('click');
-      expect(changeFn).toHaveBeenCalledTimes(1);
-    });
     it('TimeSelect open DropDrown should work well', () => {
       const wrapper = renderTimeSelect({ className: 'custom-name' });
       expect(wrapper.find('.time-select')).toHaveClassName('custom-name');
       expect(wrapper.find('Dropdown').prop('visible')).toBeFalsy();
       wrapper.find('.time-range').simulate('click');
       expect(wrapper.find('Dropdown').prop('visible')).toBeTruthy();
-    });
-    it('TimeSelect should auto refresh', () => {
-      jest.useFakeTimers();
-      const changeFn = jest.fn();
-      const wrapper = renderTimeSelect({ strategy: 'seconds:5', onChange: changeFn });
-      jest.advanceTimersByTime(6000);
-      expect(changeFn).toHaveBeenCalledTimes(1);
-      wrapper.setProps({
-        strategy: 'off',
-      });
-      jest.advanceTimersByTime(6000);
-      expect(changeFn).toHaveBeenCalledTimes(1);
-      wrapper.unmount();
     });
     it('TimeSelect Child Comp method should work well', () => {
       const changeFn = jest.fn();
